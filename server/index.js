c2b215234f1a26b58911ca6e5278960114a5868d
qaz-creator <codewarcode@protonmail.com>
2020-10-13 16:41:27 -0400

    initial

---
+++ server/index.js
@@ -0,0 +1,87 @@
+const express = require('express')
+const pool = require('./db')
+const app = express()
+const cors = require('cors')
+const PORT = process.env.PORT || 5000
+const path = require('path')
+// process.env.PORT
+
+// middelware
+app.use(cors())
+app.use(express.json())
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname,'client/build')))
+}
+// Route
+
+// create a todo
+app.post('/todos', async (req, res) => {
+  try {
+    const { description } = req.body
+    const newTodo = await pool.query(
+      'INSERT INTO todo (description) VALUES($1) RETURNING *',
+      [description],
+      //   table name, item name, placeholder(allow us to put some vars instead of it), the acual value
+    )
+    res.json(newTodo.rows[0])
+  } catch (err) {
+    console.error(err.message)
+  }
+})
+
+//get all todos
+app.get('/todos', async (req, res) => {
+  try {
+    const allTodos = await pool.query('SELECT * FROM todo')
+    res.json(allTodos.rows)
+  } catch (err) {
+    console.error(err.message)
+  }
+})
+
+// get a specific todo
+app.get('/todos/:id', async (req, res) => {
+  try {
+    // console.log(req.params)
+    const { id } = req.params
+    const todo = await pool.query('SELECT * FROM todo WHERE todo_id=$1', [id])
+    res.json(todo.rows[0])
+  } catch (error) {
+    console.error(err.message)
+  }
+})
+
+// update a todo
+app.put('/todos/:id', async (req, res) => {
+  try {
+    console.log(req.params)
+    const { id } = req.params
+    const { description } = req.body
+    const updateTodo = await pool.query(
+      'UPDATE todo SET description = $1 WHERE todo_id=$2',
+      [description, id],
+    )
+    res.json('todo is updated')
+  } catch (err) {
+    console.error(err.message)
+  }
+})
+
+// delete a todo
+
+app.delete('/todos/:id', async (req, res) => {
+  try {
+    const { id } = req.params
+    const deleteTod = await pool.query('DELETE FROM todo WHERE todo_id=$1', [
+      id,
+    ])
+    res.json('Todo was deleted')
+  } catch (err) {
+    console.error(err.message)
+  }
+})
+
+app.listen(PORT, () => {
+  console.log(`Server has started on port ${PORT}`)
+})
